5806f1090be8ca7e8684814fb7e29e2f883001d7
mhernandezit <mhernandezit@gmail.com>
2019-02-17 17:11:00 -0500

    Tasks 1 to 4 complete - starting extra credit

--- assignment3.py
+++ assignment3.py
@@ -1,43 +1,73 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 """ Assignment 3 - Using RegEx to parse a CSV file """
+from __future__ import division
+from urllib2 import Request, urlopen, URLError, HTTPError
 import csv
 import re
 import argparse
 import sys
+import logging
 from pprint import pprint as pp
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--url', help='URL to lookup', required=no, default='http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv')
-	try:
-		csvdata = downloadData(args.url)
-	except HTTPError as h:
-		logger.error(h)
-		sys.exit()
-	except URLError:
-		logger.error('Unable to retrieve CSV file')
-		sys.exit()
-	weblog = processData(csvdata)
-	imageSearch(weblog)
-	browserSearch(weblog)
-	timeSearch(weblog)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', help='URL to lookup', default='http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv')
+    args = parser.parse_args()
+    try:
+        csvdata = downloadData(args.url)
+    except HTTPError as h:
+        logger.error(h)
+        sys.exit()
+    except URLError:
+        logger.error('Unable to retrieve CSV file')
+        sys.exit()
+    weblog = processData(csvdata)
+    imageSearch(weblog)
+    browserSearch(weblog)
+    timeSearch(weblog)
+
+def downloadData(url):
+    """Download the CSV at the url provided, return a CSV reader object"""
+    req = Request(url)
+    response = urlopen(req)
+    return csv.DictReader(response, fieldnames = ("filepath","datetime","browser","status","request_size"))
 
 def processData(datafile):
-	logDict = {}
-	return logDict
+    dict_list = []
+    for line in datafile:
+        dict_list.append(line)
+    return dict_list
 
 def imageSearch(datafile):
-	pp("imageSearch placeholder")
+    images = 0
+    for row in datafile:
+        for k,v in row.items():
+            if k == 'filepath':
+                if re.search('\.(jpg|png|gif|jpeg)', v, re.IGNORECASE):
+                    images += 1
+    print('Image requests account for {}% of all requests').format(images/len(datafile)*100)
 
 def browserSearch(datafile):
-	pp("browserSearch placeholder")
+    browsers = {'firefox': 0, 'chrome': 0, 'ie': 0, 'safari': 0}
+    for row in datafile:
+        for k, v in row.items():
+            if k == 'browser':
+                if re.search('firefox', v , re.IGNORECASE):
+                    browsers['firefox'] += 1
+                if re.search('chrome', v, re.IGNORECASE):
+                    browsers['chrome'] += 1
+                if re.search('ie', v, re.IGNORECASE):
+                    browsers['ie'] += 1
+                if re.search('safari', v, re.IGNORECASE):
+                    browsers['safari'] += 1
+    print max(browsers, key=browsers.get)
 
 def timeSearch(datafile):
-	pp("timeSearch placeholder")
+    print "timesearch"
 
 if __name__ == '__main__':
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger('assignment3')
     logger.setLevel(logging.ERROR)
     try:
         logFile = logging.FileHandler('errors.log')
@@ -47,4 +77,4 @@ if __name__ == '__main__':
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logFile.setFormatter(formatter)
     logger.addHandler(logFile)
-	main()
+    main()
